0060cd17f33458720d4c8f53bbcda5fd8684662b
Král <jakub.kral@allianz.com>
2026-02-25 13:08:46 +0100

    fluent training

--- src/pages/dashboard_page.ts
+++ src/pages/dashboard_page.ts
@@ -1,24 +1,32 @@
 import { expect, Locator, Page } from "@playwright/test";
-
+import { LoginPage } from "./login_page";
 
 export class DashboardPage {
-    readonly page: Page;
-    readonly profileButton: Locator;
-    readonly logoutButton: Locator;
-    readonly alertIcon: Locator;
+  readonly page: Page;
+  readonly profileButton: Locator;
+  readonly logoutButton: Locator;
+  readonly alertIcon: Locator;
 
-    constructor (page: Page) {
-        this.page = page;
-        this.profileButton = page.locator('#user_dropdown');
-        this.logoutButton = page.locator("#logout");
-        this.alertIcon = page.locator("#user_notifications_report");
-    }
+  constructor(page: Page) {
+    this.page = page;
+    this.profileButton = page.locator("#user_dropdown");
+    this.logoutButton = page.locator("#logout");
+    this.alertIcon = page.locator("#user_notifications_report");
+  }
 
-    async logout() {
-        await expect(this.alertIcon).toBeVisible(); //počká an zobrazení informační alert ikony kvůli plnému načtení stránky
-        await this.profileButton.click();
-        await this.logoutButton.click();
-    }
+  async clickProfile() {
+    await expect(this.alertIcon).toBeVisible(); // ? Počká na zobrazení alert ikony - kvůli plnému načtení stránky
+    await this.profileButton.click();
+    return this;
+  }
 
+  async clickLogout() {
+    await this.logoutButton.click();
+    return new LoginPage(this.page);
+  }
 
+  async logout() {
+    await this.clickProfile();
+    return await this.clickLogout();
+  }
 }
